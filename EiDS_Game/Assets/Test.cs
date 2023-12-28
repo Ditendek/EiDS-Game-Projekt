123dcbbcf8c007292976b4921ee91cfd8831d24f
TamiVN <hoang.tan@st.ovgu.de>
2023-12-28 04:21:41 +0100

    asset imports

---
+++ EiDS_Game/Assets/Test.cs
@@ -0,0 +1,19 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Test : MonoBehaviour
+{
+    // Start is called before the first frame update
+    Rigidbody2D rb;
+    void Start()
+    {
+        rb = GetComponent<Rigidbody2D>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        rb.AddForce(new Vector3(0.2f,0.2f,10f));
+    }
+}
