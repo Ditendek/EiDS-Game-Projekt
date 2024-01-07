1465382706d4b693b47d1d35468824731d6dab58
Ditendek <132198129+Ditendek@users.noreply.github.com>
2024-01-07 17:07:18 +0100

    rng

--- EiDS_Game/Assets/Main/Scripts/Dungeongeneration/DungeonBuilder.cs
+++ EiDS_Game/Assets/Main/Scripts/Dungeongeneration/DungeonBuilder.cs
@@ -16,6 +16,8 @@ public class DungeonBuilder : MonoBehaviour {
         _dungeonLayoutGenerator = GetComponent<TreeLikeDungeonLayoutGenerator>();
         _roomLayoutGenerator = GetComponent<RoomFromPrefabLayoutGenerator>();
 
+        seed = Random.Range(0, int.MaxValue);
+
         Random.InitState(seed);
         randomState = Random.state;
 
