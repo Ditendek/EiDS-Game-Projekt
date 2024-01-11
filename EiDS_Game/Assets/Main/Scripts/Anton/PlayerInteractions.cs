e69323ab836a7d23685d65659b310062a8b3ff76
Anton_P <anton.paschke@st.ovgu.de>
2024-01-11 16:18:46 +0100

    counter für powerups

--- EiDS_Game/Assets/Main/Scripts/Anton/PlayerInteractions.cs
+++ EiDS_Game/Assets/Main/Scripts/Anton/PlayerInteractions.cs
@@ -1,16 +1,23 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class PlayerInteractions : MonoBehaviour
 {
+    private int bowcounter;
+    private int swordcounter;
 
     public Arrow arrowReference;
     public PlayerAttack attackReference;
 
+    public Text bowText;
+    public Text swordText;
+
     void Start()
     {
-     
+        bowcounter = 0;
+        swordcounter = 0;
     }
 
     void Update()
@@ -23,13 +30,17 @@ public class PlayerInteractions : MonoBehaviour
         if (collision.gameObject.CompareTag("PowerUp_Bow"))
         {
             Destroy(collision.gameObject);
+            bowcounter += 1;
             arrowReference.damageIncrease += 2;
+            bowText.text = "+" + bowcounter;
         }
 
         if (collision.gameObject.CompareTag("PowerUp_Sword"))
         {
             Destroy(collision.gameObject);
+            swordcounter += 1;
             attackReference.damageIncrease += 2;
+            swordText.text = "+" + swordcounter;
         }
     }
 }
